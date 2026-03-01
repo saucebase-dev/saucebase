dcbf579d44ad09e72c17e6a9cf166148ed2d5283
roble <renanroble@gmail.com>
2026-03-01 07:59:21 +0000

    refactor: reorganize head section and improve Google Tag Manager integration

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,22 +1,50 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Sauce Base') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">
+    <title inertia>{{ config('app.name', 'Sauce Base') }}</title>
 
-        <!-- Scripts -->
-        @routes
-        @vite(['resources/js/app.ts'])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased bg-background text-foreground dark:bg-background dark:text-foreground">
-        @inertia
-    </body>
+    <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-WTRCPLVS');
+    </script>
+    <!-- End Google Tag Manager -->
+
+    <!-- Scripts -->
+    @routes
+    @vite(['resources/js/app.ts'])
+    @inertiaHead
+</head>
+
+<body class="font-sans antialiased bg-background text-foreground dark:bg-background dark:text-foreground">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WTRCPLVS" height="0" width="0"
+            style="display:none;visibility:hidden" />
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    @inertia
+</body>
+
 </html>
