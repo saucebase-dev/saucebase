ef2ed5c64d2a7bbdc2c98f1541b0e991fd9dae00
roble <renanroble@gmail.com>
2026-03-22 19:37:03 +0000

    feat: add error messages and translations for status codes in Error.vue and pt_BR.json

--- resources/js/pages/Error.vue
+++ resources/js/pages/Error.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button';
 import { Head, Link } from '@inertiajs/vue3';
+import { trans } from 'laravel-vue-i18n';
 import { computed } from 'vue';
 
 const props = defineProps({ status: Number });
@@ -8,22 +9,26 @@ const props = defineProps({ status: Number });
 const title = computed(() => {
     return (
         {
-            503: '503: Service Unavailable',
-            500: '500: Server Error',
-            404: '404: Page Not Found',
-            403: '403: Forbidden',
-        }[props.status as number] ?? 'Error'
+            503: trans('503: Service Unavailable'),
+            500: trans('500: Server Error'),
+            404: trans('404: Page Not Found'),
+            403: trans('403: Forbidden'),
+        }[props.status as number] ?? trans('Error')
     );
 });
 
 const description = computed(() => {
     return (
         {
-            503: 'Sorry, we are doing some maintenance. Please check back soon.',
-            500: 'Whoops, something went wrong on our servers.',
-            404: 'Sorry, the page you are looking for could not be found.',
-            403: 'Sorry, you are forbidden from accessing this page.',
-        }[props.status as number] ?? 'An error occurred.'
+            503: trans(
+                'Sorry, we are doing some maintenance. Please check back soon.',
+            ),
+            500: trans('Whoops, something went wrong on our servers.'),
+            404: trans(
+                'Sorry, the page you are looking for could not be found.',
+            ),
+            403: trans('Sorry, you are forbidden from accessing this page.'),
+        }[props.status as number] ?? trans('An error occurred.')
     );
 });
 </script>
@@ -50,17 +55,8 @@ const description = computed(() => {
 
             <div class="space-y-4">
                 <Button as-child>
-                    <Link :href="route('dashboard')"> Go to Dashboard </Link>
+                    <Link :href="route('index')"> {{ $t('Go to Home') }} </Link>
                 </Button>
-
-                <div>
-                    <Link
-                        :href="route('index')"
-                        class="text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
-                    >
-                        ← Back to Home
-                    </Link>
-                </div>
             </div>
         </div>
     </div>
