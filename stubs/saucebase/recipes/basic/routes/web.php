f2f910840bcaf300b2267b314ecb84b70b0f6730
roble <renanroble@gmail.com>
2026-04-24 20:17:17 +0100

    feat: update module structure and routing for improved organization and functionality

--- stubs/saucebase/recipes/basic/routes/web.php
+++ stubs/saucebase/recipes/basic/routes/web.php
@@ -3,4 +3,6 @@
 use ___MODULE_NAMESPACE___\___Module___\Http\Controllers\___Module___Controller;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('{module-}', ___Module___Controller::class);
+Route::middleware('web')->group(function () {
+    Route::get('{module-}', ___Module___Controller::class)->name('{module-}.index');
+});
