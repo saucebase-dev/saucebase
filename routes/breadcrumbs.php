55dae8ddf64e726707f44322b38f8e9f9dad039a
roble <renanroble@gmail.com>
2026-04-25 10:23:02 +0100

    feat: remove commented line for dashboard in breadcrumbs.php for cleaner code

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -3,7 +3,6 @@
 use Saucebase\Breadcrumbs\Breadcrumbs;
 use Saucebase\Breadcrumbs\Generator as Trail;
 
-// Dashboard (home)
 Breadcrumbs::for('dashboard', function (Trail $trail) {
     $trail->push('dashboard', route('dashboard'));
 });
